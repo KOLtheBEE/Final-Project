5d33a5709cc2ed450e3c2ce91e19644b4dfa37d5
Gamerplayer22 <91217607+Gamerplayer22@users.noreply.github.com>
2024-05-02 04:14:50 +0000

    test updated

--- test_main.py
+++ test_main.py
@@ -2,11 +2,10 @@ import pytest
 from main import rps
 
 
-@pytest.mark.parametrize("user_input",
-                         ["1"])  # Provide the desired input value
-def test_rps(monkeypatch, capsys, user_input):
+@pytest.mark.parametrize("user", ["1"])  # Provide the desired input value
+def test_rps(monkeypatch, capsys, user):
   # Use monkeypatch to replace the input function
-  monkeypatch.setattr('builtins.input', lambda _: user_input)
+  monkeypatch.setattr('builtins.input', lambda _: user)
 
   # Call the function you want to test
   rps()
