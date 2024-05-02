d1a3ec550b6c1f21a4258030e29e24d5c1079166
Gamerplayer22 <91217607+Gamerplayer22@users.noreply.github.com>
2024-05-02 04:08:20 +0000

    updated test

--- test_main.py
+++ test_main.py
@@ -1,26 +1,19 @@
-import unittest
-from io import StringIO
-from unittest.mock import patch
-
+import pytest
 from main import rps
 
 
-class TestMain(unittest.TestCase):
+@pytest.mark.parametrize("user_input",
+                         ["1"])  # Provide the desired input value
+def test_rps(monkeypatch, capsys, user_input):
+  # Use monkeypatch to replace the input function
+  monkeypatch.setattr('builtins.input', lambda _: user_input)
 
-  @patch('builtins.input', side_effect=['r'])
-  @patch('random.choice', return_value='r')
-  @patch('sys.stdout', new_callable=StringIO)
-  def test_rps_equal(self, mock_stdout, mock_random_choice, mock_input):
-    expected_output = ("What is your choice?\nPlayer: r\tComputer: r.\nIt's a tie!\n\n")
-    
-    rps()
+  # Call the function you want to test
+  rps()
 
-    actual_output = mock_stdout.getvalue()
+  # Capture the printed output
+  captured = capsys.readouterr()
 
-    print(actual_output)
-
-    self.assertEqual(actual_output, expected_output)
-
-
-if __name__ == '__main__':
-  unittest.main()
+  # Assert against the expected output
+  assert "Select a game (1-3):" in captured.out
+  # Add more assertions as needed
